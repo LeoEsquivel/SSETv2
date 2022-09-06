1a0c213d102f911471aaaa8042a2dd349f08913b
Leo <danielesquivel00.lded@gmail.com>
2022-09-06 11:17:03 -0500

    ADD lazyload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then( m => m.AdminModule)
+  },
+  {
+    path: 'enlace',
+    loadChildren: () => import('./enlace/enlace.module').then( m => m.EnlaceModule)
+  },
+  {
+    path: 'planeacion',
+    loadChildren: () => import('./planeacion/planeacion.module').then( m => m.PlaneacionModule)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
